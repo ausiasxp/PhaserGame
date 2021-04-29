660ee36c40e1d3122430e7e5ae80c8714d3e771f
VnssJT <64807616+VnssJT@users.noreply.github.com>
2021-04-29 18:14:08 +0200

    assets de botones play y about

--- js/welcome.js
+++ js/welcome.js
@@ -6,8 +6,23 @@ let initialState = {
 
 function loadAssets(){
     game.load.image('bg', 'assets/images/parallax-forest.png');
+    game.load.image('playImg', 'assets/images/button_play.png');
+    game.load.image('aboutImg', 'assets/images/button_about.png');
 }
 
 function displayScreen(){
+    game.input.enabled = true;
     game.add.image(0, 0, 'bg');
+
+    showAuthors();
+}
+
+function showAuthors(){
+    let msgAuthors = 'Ausias García Torres\nVanessa Jiménez Tarazona\nJosaem Silva Sanmiguel';
+    let styleAuthors = {
+        font: '12pt Sniglet',
+        fill: '#FFFFFF'
+    };
+
+    game.add.text(125, game.world.height / 6 + 60, msgAuthors, styleAuthors);
 }
